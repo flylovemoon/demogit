17bea1656ae9f88a0a52c46b368158e378ad575a
gtf <gtf@qq.com>
2022-12-08 16:58:10 +0800

    加入日期属性

--- src/main/java/com/example/demogit/eneity/User.java
+++ src/main/java/com/example/demogit/eneity/User.java
@@ -1,7 +1,10 @@
 package com.example.demogit.eneity;
 
+import java.util.Date;
+
 public class User {
     private Integer id;
     private String username;
     private String password;
+    private Date bir;
 }
